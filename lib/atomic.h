ggow/libmetal

commit 81f85dc244d05448e26aa674301065a1220b4539

atomic: use cpp atomic if cpp compiler is used
Use cpp atomic if cpp compiler is used as the
c atomic definition doesn't work with the cpp
atomic definition.

Signed-off-by: Wendy Liang <wendy.liang@xilinx.com>

lib/atomic.h
@@ -17,6 +17,8 @@
 #if defined(HAVE_STDATOMIC_H) && !defined(__STDC_NO_ATOMICS__) && \
 	!defined(__cplusplus)
 # include <stdatomic.h>
+#elif defined(__cplusplus)
+# include <atomic>
 #elif defined(__GNUC__)
 # include <metal/compiler/gcc/atomic.h>
 #else

commit ed356b51aa78b710bad6894808c47bf277621d53

lib: atomic: revert c++ compilation issue
Commit 81f85dc caused compilation errors when compiling libmetal
applications using C++. Reverting this commit.

Signed-off-by: Ben Levinsky <ben.levinsky@xilinx.com>

lib/atomic.h
@@ -17,8 +17,6 @@
 #if defined(HAVE_STDATOMIC_H) && !defined(__STDC_NO_ATOMICS__) && \
 	!defined(__cplusplus)
 # include <stdatomic.h>
-#elif defined(__cplusplus)
-# include <atomic>
 #elif defined(__GNUC__)
 # include <metal/compiler/gcc/atomic.h>
 #else